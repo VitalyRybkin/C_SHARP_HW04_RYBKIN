4ac4f8a7e1e04e5161da746d5e2bb71eff7ec3db
Vitaly <vitaly.rybkin@gmail.com>
2022-12-05 11:18:36 +0300

    added negative power

--- TASK01_25/Program.cs
+++ TASK01_25/Program.cs
@@ -8,13 +8,36 @@
 Console.Clear();
 
 Console.Write("Enter num to be powered: ");
-int num = Convert.ToInt32(Console.ReadLine());
+string get_string = CheckInput(Console.ReadLine() ?? String.Empty);
+int num = Convert.ToInt32(get_string);
 Console.Write("Enter power: ");
-int pow = Convert.ToInt32(Console.ReadLine());
-int res = num;
+get_string = CheckInput(Console.ReadLine() ?? String.Empty);
+int pow = Convert.ToInt32(get_string);
+double res = num;
 
-for (int i = 2; i <= pow; i ++) {
-    res = res * num;
+if (pow > 0) {
+    for (int i = 2; i <= pow; i ++) {
+        res = res * num;
+    }
 }
-
+else {
+    for (int i = 2; i <= Math.Abs(pow); i ++) {
+        res = res * num;
+    }
+    res = 1 / res;
+}
 Console.WriteLine($"{num} to the power of {pow} is " + res);
+
+string CheckInput (string get_string) {
+    while (true) {
+        if (get_string == "Q") Environment.Exit(0);
+        if (int.TryParse(get_string, out int num)) {
+        return get_string;
+        }
+        else {
+                Console.Write("\nThis is not an int! ");
+                Console.Write("Try again or type 'Q': ");
+                get_string = Console.ReadLine() ?? "";
+        }
+    }
+}  
