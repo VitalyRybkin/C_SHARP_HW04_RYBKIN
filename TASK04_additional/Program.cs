3618ce9130adf8dfe12ecc9d06204d88bb950634
Vitaly <vitaly.rybkin@gmail.com>
2022-12-05 13:21:18 +0300

    additional task added

---
+++ TASK04_additional/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Дополнительная задача (задача со звёздочкой): 
+Напишите программу, которая на вход принимает радиус круга и находит его площадь округленную до целого числа, необходимо вывести максимальную цифру в полученном округлённом значении площади круга.
+10 -> 4
+20 -> 7
+30 -> 8
+*/
+
+Console.Clear();
+
+Console.Write("Enter circle radius: ");
+string get_string = CheckInput(Console.ReadLine() ?? String.Empty);
+double radius = Convert.ToDouble(get_string);
+double s = 0;
+int max_digit = 0;
+
+s = Math.Round(Math.PI * Math.Pow(radius, 2));
+
+while (s != 0) {
+    if (max_digit < Convert.ToInt32(s % 10)) max_digit = Convert.ToInt32(s % 10);
+    s /= 10;
+}
+
+Console.WriteLine(max_digit);
+
+
+string CheckInput (string get_string) {
+    while (true) {
+        if (get_string == "Q") Environment.Exit(0);
+        if (double.TryParse(get_string, out double num)) {
+        return get_string;
+        }
+        else {
+                Console.Write("\nThis is not an int! ");
+                Console.Write("Try again or type 'Q': ");
+                get_string = Console.ReadLine() ?? "";
+        }
+    }
+}  
